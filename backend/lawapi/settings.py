74325919e1f0948707e5f6e237f1d836054ed976
Kunal <kunal.mittal@iiitb.ac.in>
2025-07-19 11:54:33 +0530

    main commit

--- backend/lawapi/settings.py
+++ backend/lawapi/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'lawapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'law_portfolio',
+        'USER': 'root',
+        'PASSWORD': 'kunal0025',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
